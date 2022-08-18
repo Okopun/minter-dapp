4c71725dcf9d862b01239e611df89b54ad070c02
Oliver Kopun <o.kopun@gmail.com>
2022-08-18 20:49:52 +0200

    changed description

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x5Ebd1fE67E58A49152B2997D949826E131C9D9a2"; // Replace with your own contract address
 const chain = 'polygon'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Polygon Trees NFT project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Polygon Trees is a collection of 1000 saplings growing on the polygon network. Each NFT represents a physical tree I plant in real life.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
